c671b7cde6a5336091f36572dc2ffc09bd70cf9b
Josh Allmann <joshua.allmann@gmail.com>
2013-11-05 23:24:21 -0800

    Calculate dependency ordering for types.

--- netser.ml
+++ netser.ml
@@ -1,5 +1,7 @@
 open Netser_types
 
+module SS = Set.Make(String)
+
 let print_elem e =
     let p_t t =
         match t with
@@ -27,6 +29,67 @@ let parse s =
     let s = Lexing.from_string s in
     Netser_parser.parse_sexpr (Netser_lexer.tokens) s
 
+
+let rec tree_deps = function
+    | Ast_elem (Ast_ident(_, Identifier s)) -> [s]
+    | Ast_product l -> List.flatten (List.map tree_deps l)
+    | Ast_sum l -> List.flatten (List.map tree_deps l)
+    | _ -> []
+
+let outgoing_edges s =
+    let trees = List.map (fun (_, tree) -> tree) s in
+    List.map tree_deps trees
+
+let incoming_edges l =
+    let incoming : (string, string list) Hashtbl.t = Hashtbl.create 100 in
+    let outgoings = outgoing_edges l in
+    let set_empty (name, _) = Hashtbl.add incoming name [] in
+    let add2in (name, _) edges =
+        let add2hash x =
+            try let s = Hashtbl.find incoming x in
+            Hashtbl.replace incoming x (name::s)
+            with exn -> () in (* occurs with undefined types *)
+        List.iter add2hash edges in
+    let in2list (name, _) = Hashtbl.find incoming name in
+    List.iter set_empty l;
+    List.iter2 add2in l outgoings;
+    List.map in2list l
+
+let dep_order l =
+    (* create hashmap of name -> edges *)
+    let out : (string, string list) Hashtbl.t = Hashtbl.create 100 in
+    let edges = outgoing_edges l in
+    List.iter2 (fun (name, _) e -> Hashtbl.add out name e) l edges;
+
+    let add s value = s := SS.add value !s in
+    let remove s value = s := SS.remove value !s in
+    let contains s value = SS.exists (fun x -> x = value) !s in
+
+    let marked = ref SS.empty in
+    let temp = ref SS.empty in
+    let unmarked = ref SS.empty in
+    let order = ref [] in
+
+    (* topological sort *)
+    List.iter (fun (name, _) -> (add unmarked name)) l;
+    let rec visit name =
+        if contains temp name then raise (Failure "Not a DAG");
+        if not (contains marked name) then begin
+            try
+                let n_edges = Hashtbl.find out name in
+                add temp name;
+                List.iter visit n_edges;
+                remove temp name;
+                add marked name;
+                order := name :: !order
+            with Not_found -> (); (* nonexistent name, what to do? *)
+        end in
+    while not (SS.is_empty !unmarked) do
+        let name = SS.choose !unmarked in
+        remove unmarked name;
+        visit name
+    done;
+    List.rev !order
 
 let normalize l =
     (* converts (a b c (def) (ghi jkl) m n o) to
