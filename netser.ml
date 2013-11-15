30abd2b065c811f32b0cdaef4513f252d41eb499
Josh Allmann <joshua.allmann@gmail.com>
2013-11-15 14:02:51 -0800

    Generate serializers.

--- netser.ml
+++ netser.ml
@@ -95,9 +95,112 @@ let print_type types =
     let strs = List.map gen_types types in
     String.concat "\n\n" strs
 
+let elem_decon = function
+    | Ast_literal (_, _) -> ""
+    | Ast_ident (None, _, _) -> raise (Failure "Empty ident; fixup?")
+    | Ast_ident (Some s, _,_) -> s
+
+let rec deconstruct_data = function
+    | Ast_elem e -> elem_decon e
+    | Ast_product p -> prod_decon p
+    | _ -> ""
+and prod_decon p =
+    let data = List.map deconstruct_data p in
+    let ne = nonempty data in
+    let str = String.concat ", " ne in
+    if List.length ne > 1 then Printf.sprintf "(%s)" str else str
+
+let deconstruct name data =
+    let d = deconstruct_data data in
+    if "" <> d then Printf.sprintf "let %s = %s in\n" d name else d
+
+let prim_writer = function
+    | PRIM_UINT8 -> "Netser_io.write_u8"
+    | PRIM_UINT16 -> "Netser_io.write_u16"
+    | PRIM_INT -> "Netser_io.write_int"
+    | PRIM_INT32 -> "Netser_io.write_i32"
+    | PRIM_CHAR -> "Netser_io.write_char"
+    | PRIM_FLOAT -> "Netser_io.write_float"
+
+let id_writer s = s^"_writer"
+
+let type_writer = function
+    | Prim p -> prim_writer p
+    | Identifier s -> id_writer s
+
+let write_literal lit typ =
+    let (Ast_int_literal i) = lit in
+    let value = string_of_int i in
+    let writer_str = type_writer typ in
+    Printf.sprintf "%s b %s" writer_str value
+
+let write_ident name typ count =
+    let writer_str = type_writer typ in
+    match count with
+    | Count_literal 1 -> Printf.sprintf "%s b %s" writer_str name
+    | _ -> Printf.sprintf "List.map (%s b) %s" writer_str name
+
+let write_elem = function
+    | Ast_literal (a, b) -> write_literal a b
+    | Ast_ident (Some a, b, c) -> write_ident a b c
+    | Ast_ident (None, _, _) -> raise (Failure "Empty ident; fixup?")
+
+let rec data_writer name = function
+    | Ast_elem e -> write_elem e
+    | Ast_product p -> String.concat ";\n" (List.map (data_writer name ) p)
+    | Ast_sum s -> write_sum name s
+    | Ast_pound (typ, p) -> write_pound name typ p
+
+and write_sum name s =
+    let cap = upcase name in
+    let str = Printf.sprintf "match %s with\n" name in
+    let gen_ctr i x =
+        let w = data_writer name x in
+        let d = deconstruct_data x in
+        let str = Printf.sprintf "%s%d %s" cap i d in
+        Printf.sprintf "%s -> %s" str w in
+    let str = str ^ String.concat "\n| " (List.mapi gen_ctr s) in
+    Printf.sprintf "(%s)" str
+
+and write_pound name typ p =
+    let calc_den = function
+    | Identifier i -> raise (Failure (Printf.sprintf "Can't have ident %s as pound-type" i))
+    | Prim PRIM_FLOAT -> raise (Failure "Can't have float as pound-type")
+    | Prim PRIM_UINT8 | Prim PRIM_CHAR -> 1
+    | Prim PRIM_UINT16 -> 2
+    | Prim PRIM_INT | Prim PRIM_INT32 -> 4 in
+    let str = "let contents =\nlet b = Netser_io.create () in\n" in
+    let str = str ^ (data_writer name p) in
+    let str = str ^ ";\nNetser_io.contents b in\n" in
+    let len = Printf.sprintf "(int_of_float (ceil (float_of_int (String.length contents)/.%d.0)))" (calc_den typ) in
+    Printf.sprintf "%s%s b %s;\nNetser_io.write_string b contents" str (type_writer typ) len
+
+let signature name written is_recursive =
+    let t = id2type name in
+    let prefix = if !written then "" else
+        (let p = "let " ^ (if is_recursive then "rec " else "") in
+        written := true; p) in
+    let w = id_writer name in
+    Printf.sprintf "%s %s b (%s:%s) =\n" prefix w name t
+
+let print_writers types =
+    let write_list l =
+        let prefix_written = ref false in
+        let is_rec = List.length l > 1 in
+        let write (n, d) =
+            let sig_line = signature n prefix_written is_rec in
+            let decon_line = deconstruct n d in
+            let write_block = data_writer n d in
+            sig_line^decon_line^write_block in
+        let strs = List.map write l in
+        String.concat "\nand " strs in
+    String.concat "\n\n" (List.map write_list types)
+
 let initialize s =
     let parsetree = parse s in
     let stuff x = fixup_elems x in
     let ast = List.map stuff parsetree in
     let order = Netser_ordering.type_order ast in
-    print_type order
+    let s0 = print_type order in
+    let s1 = print_writers order in
+    Printf.sprintf "%s\n\n%s\n" s0 s1
